d943175fef1605be919369e445a46b793c25b94a
Ester Cortes <002escor@gmail.com>
2023-05-20 21:41:19 +0200

    making the bunny rotate somewhat

--- src/components/CanvasComponent.js
+++ src/components/CanvasComponent.js
@@ -1,24 +1,61 @@
-import { RoundedBox } from "@react-three/drei";
 import { useRef } from "react";
 import React from "react";
 import { useFrame } from "@react-three/fiber";
+import {
+  Environment,
+  OrbitControls,
+  RoundedBox,
+  Scroll,
+  ScrollControls,
+} from "@react-three/drei";
+import { BoxGeometry } from "three";
+import Hero from "./Hero";
+import Nav from "./Nav";
+import { About } from "./About";
+import Footer from "./Footer";
+import { Bunny } from "./Bunny";
 
 const CanvasComponent = () => {
-  const boxRef = useRef();
-
-  useFrame((state, delta) => {
-    boxRef.current.rotation.y += delta;
-  });
-
   return (
     <>
       <color attach="background" args={["#333333"]} />
-      <ambientLight intensity={1} />
-      <mesh>
-        <RoundedBox args={[1, 1, 1]} radius={0.05} smoothness={4} ref={boxRef}>
-          <meshPhongMaterial color="red" />
-        </RoundedBox>
-      </mesh>
+
+      <ScrollControls pages={3} damping={0.1}>
+        {/* Canvas contents in here will *not* scroll, but receive useScroll! */}
+        <ambientLight intensity={0.3} />
+        <spotLight
+          position={[0, 25, 0]}
+          angle={1.3}
+          penumbra={1}
+          castShadow
+          intensity={2}
+          shadow-bias={-0.0001}
+        />
+        {/* <Environment
+          background={false} // can be true, false or "only" (which only sets the background) (default: false)
+          preset="warehouse"
+          scene={undefined} // adds the ability to pass a custom THREE.Scene, can also be a ref
+          encoding={undefined} // adds the ability to pass a custom THREE.TextureEncoding (default: THREE.sRGBEncoding for an array of files and THREE.LinearEncoding for a single texture)
+        /> */}
+        <mesh scale={0.25}>
+          <Bunny position={[-7, -10, 0]} />
+        </mesh>
+        <Scroll>
+          {/* Canvas contents in here will scroll along */}
+          {/* <RoundedBox args={[1, 1, 1]} radius={[0.05]} smoothness={4}>
+            <meshPhongMaterial color={"f2f2f2"} wireframe />
+          </RoundedBox> */}
+        </Scroll>
+        <Scroll html style={{ width: "100%" }}>
+          {/* DOM contents in here will scroll along */}
+          <Nav />
+          <Hero />
+          <h1>second page</h1>
+          <About style={{ position: "absolute", top: "100vh" }} />
+          <h1 style={{ position: "absolute", top: "200vh" }}>third page</h1>
+          <Footer />
+        </Scroll>
+      </ScrollControls>
     </>
   );
 };
